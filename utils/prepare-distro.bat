86e70f3bac174973c364ae1d6b1f4de51fa74490
Patrick Percot <patrick.percot@morbihan.fr>
2017-03-30 15:28:08 +0200

    Initialisation du projet en version 1.0.0

---
+++ utils/prepare-distro.bat
@@ -0,0 +1,32 @@
+@echo off
+REM **********************************************************
+REM
+REM Préparation des exécutables du connecteur Asalaé
+REM
+REM **********************************************************
+REM
+
+
+REM On se place dans le répertoire parent
+cd ..
+
+REM Copie des exécutables Java 
+echo Suppression des exécutables java 
+if not exist lib mkdir lib
+del /Q lib\*
+
+echo Copie des exécutables Java 
+copy ..\cd56-connecteur-asalae\target\lib\* lib 1>NUL
+copy ..\cd56-connecteur-asalae\target\cd56-connecteur-asalae-*.jar . 1>NUL
+
+echo Copie de la documentation
+rmdir /S /Q Documentation
+mkdir Documentation
+xcopy /S /Q /Y ..\cd56-connecteur-asalae\Documentation Documentation 1>NUL
+
+copy ..\cd56-connecteur-asalae\param.config.sample .
+
+REM On revient dans utils
+cd utils
+
+echo Copie achevée
---
+++ utils/prepare-distro.sh
@@ -0,0 +1,31 @@
+﻿#!/bin/bash
+
+# **********************************************************
+#
+# Préparation des exécutables du connecteur As@laé
+#
+# **********************************************************
+#
+
+# On se place dans le répertoire parent
+cd ..
+
+# Copie des exécutables Java 
+echo Suppression des exécutables java 
+[ ! -e lib ] && mkdir lib
+rm lib/*
+
+echo Copie des exécutables Java
+cp ../cd56-connecteur-asalae/target/lib/* lib
+cp ../cd56-connecteur-asalae/target/cd56-connecteur-asalae-*.jar lib
+
+echo Copie de la documentation
+rm -rf Documentation
+cp -r ../cd56-connecteur-asalae/Documentation .
+
+cp ../cd56-connecteur-asalae/param.config.sample .
+
+# On revient dans utils
+cd -
+
+echo Copie achevée
